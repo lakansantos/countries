8ee936b03a504589f5cfb06afa2bce76ad3fc863
Lakan Santos <rizesuoracle25@gmail.com>
2023-10-30 16:39:14 +0800

    Added a language format for multiple languages

--- app/components/Layouts/LayoutCountriesCard.tsx
+++ app/components/Layouts/LayoutCountriesCard.tsx
@@ -10,6 +10,29 @@ type LayoutCountriesCardPropsType = {
 const LayoutCountriesCard = (props: LayoutCountriesCardPropsType) => {
   const {country_name, flag, languages, population} = props;
 
+  //To get the name of languages
+  const languageNames = languages?.map((language) => language.name);
+
+  //to get the length of languages names
+  const numberOfLanguages = languageNames.length;
+
+  //to get the last Index of languages
+  const lastIndexLanguage = numberOfLanguages - 1;
+  //to get the last language that we will later on slice for this format "English, and Filipino"
+  const lastLanguage = languageNames[lastIndexLanguage];
+
+  //removing the last language
+  const filteredLanguages = languageNames.slice(0, lastIndexLanguage);
+
+  const hasMoreThanOneLanguage = languageNames.length > 1;
+
+  const formattedOneLanguage = languageNames.join(", ");
+
+  //if there's more than 1 language, we can format it with "English, and Filipino"
+  const formattedLanguages = hasMoreThanOneLanguage
+    ? `${filteredLanguages.join(", ")} and ${lastLanguage}`
+    : formattedOneLanguage;
+
   return (
     <div className=" w-[220px] h-[350px] bg-white flex flex-col my-4">
       <Image
@@ -25,7 +48,7 @@ const LayoutCountriesCard = (props: LayoutCountriesCardPropsType) => {
         priority
       />
       <p>Capital: {country_name}</p>
-      <p>Languages: {languages?.map((language) => language.name).join(", ")}</p>
+      <p>Languages: {formattedLanguages}</p>
       <p>Population: {population}</p>
     </div>
   );
