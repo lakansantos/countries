ee0f43cf722c874112446cb8b1da328e682d6597
Lakan Santos <rizesuoracle25@gmail.com>
2023-11-08 20:43:07 +0800

    Tried to get the sorted number of language used and added boilerplate for analytics

---
+++ app/modules/analytics/Analytics.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Analytics = () => {
+  return <div className="h-[80vh]">Analytics</div>;
+};
+
+export default Analytics;
--- app/modules/home/Home.tsx
+++ app/modules/home/Home.tsx
@@ -3,9 +3,40 @@ import Header from "../header/Header";
 import Countries from "../countries/Countries";
 
 import {useCountries} from "@/app/context/CountriesContext";
+import Analytics from "../analytics/Analytics";
 
 const Home = () => {
   const {data, searchedData, isLoading, error} = useCountries();
+
+  const filteredLanguages = data
+    ? Object.values(data).flatMap((country) =>
+        country.languages.map((language) => language.name)
+      )
+    : [];
+
+  const container = {};
+
+  filteredLanguages.map((language) => {
+    if (typeof container[language] === "number") {
+      container[language] += 1;
+    } else {
+      container[language] = 1;
+    }
+
+    return container;
+  });
+
+  const arrayContainer = Object.entries(container);
+
+  const sorter = arrayContainer
+    .sort((a, b) => b[1] - a[1])
+    .map((item) => {
+      const [language, count] = item;
+
+      return {language, count};
+    });
+
+  console.log(sorter);
 
   //if error, will redirect to error page
   if (error) throw Error(error.message);
@@ -17,6 +48,7 @@ const Home = () => {
         data={!!searchedData ? searchedData : data}
         isLoading={isLoading}
       />
+      <Analytics />
     </React.Fragment>
   );
 };
