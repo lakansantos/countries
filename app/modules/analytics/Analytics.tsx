ded7f99240f173df027025a0e8900e79210d9be7
Lakan Santos <rizesuoracle25@gmail.com>
2023-11-08 21:02:31 +0800

    Added function that gets top 10 languages

--- app/modules/analytics/Analytics.tsx
+++ app/modules/analytics/Analytics.tsx
@@ -1,6 +1,47 @@
 import React from "react";
 
-const Analytics = () => {
+type AnalyticsProps = {
+  data: Data[] | null;
+};
+
+const getTopTenLanguages = (data: Data[]) => {
+  const filteredLanguages = data
+    ? Object.values(data).flatMap((country) =>
+        country.languages.map((language) => language.name)
+      )
+    : [];
+
+  const container = {};
+
+  filteredLanguages.map((language) => {
+    if (typeof container[language] === "number") {
+      container[language] += 1;
+    } else {
+      container[language] = 1;
+    }
+
+    return container;
+  });
+
+  const arrayContainer = Object.entries(container);
+
+  const sorter = arrayContainer
+    .map((item) => {
+      const [language, count] = item;
+
+      return {language, count};
+    })
+    .sort((a, b) => b.count - a.count);
+
+  const topTenLanguages = sorter.slice(0, 10);
+  return topTenLanguages;
+};
+const Analytics = (props: AnalyticsProps) => {
+  const {data} = props;
+
+  const topTenLanguages = getTopTenLanguages(data);
+
+  console.log(topTenLanguages);
   return <div className="h-[80vh]">Analytics</div>;
 };
 
